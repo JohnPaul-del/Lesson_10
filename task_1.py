e96f0d9fdeea362094ba254e2197c2543bf31eb7
ivan <ivan.pavlov.alk@gmail.com>
2021-03-25 21:57:46 +0200

    Task 2 - complete

--- task_1.py
+++ task_1.py
@@ -1,21 +1,16 @@
+import numpy as np
+
 class Matrix:
     def __init__(self, matrix):
         self.matrix = matrix
 
     def __add__(self, other):
         res = []
-        summ_numbers = []
         if len(self.matrix[0]) == len(other.matrix[0]):
-            for i in range(len(self.matrix)):
-                for j in range(len(self.matrix[0])):
-                    summ = self.matrix[i][j] + other.matrix[i][j]
-                    summ_numbers.append(summ)
-                    if len(summ_numbers) == len(self.matrix):
-                        res.append(summ_numbers)
-                        summ_numbers = []
+            res = [[self.matrix[i][j] + other.matrix[i][j] for j in range(len(self.matrix[0]))]
+                   for i in range(len(self.matrix))]
         else:
-            print("ss")
-
+            pass
         return Matrix(res)
 
     def __str__(self):
@@ -24,7 +19,7 @@ class Matrix:
 
 
 matrix_1 = Matrix([[11, 15, 1], [34, 43, 23], [100, 54, 1]])
-matrix_2 = Matrix([[1, 4, 4], [15, 6, 5], [90, 67, 100]])
+matrix_2 = Matrix([[1, 4], [51, 15], [25, 90]])
 
 matrix_3 = matrix_1 + matrix_2
 print(f"{'*'*30} First Matrix {'*'*30}")
